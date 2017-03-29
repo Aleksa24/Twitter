f561d4bb2868aa660138fdb34ef4c65950abefd3
Aleksa24 <aleksaivanovic24444@gmail.com>
2017-03-29 22:41:57 +0200

    junit prvi put
    u klasi TwitterPorukaTest sam napravio metode za sve scenarije koji rade
    nakon sto sam tokom testiranja izmenio greske u klasi TwitterPoruka

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -30,7 +30,7 @@ public class TwitterPoruka {
 	 * throws link java.lang.RuntimeException ako je ime prazno ili null
 	 */
 	public void setKorisnik(String korisnik) {
-		if (korisnik==null || !korisnik.isEmpty())
+		if (korisnik==null || korisnik.isEmpty())
 			throw new RuntimeException(
 					"Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
@@ -41,7 +41,7 @@ public class TwitterPoruka {
 	 * @return vrednost poruke kao String
 	 */
 	public String getPoruka() {
-		return "poruka";
+		return this.poruka;
 	}
 	
 	/**
@@ -50,8 +50,8 @@ public class TwitterPoruka {
 	 * @throws java.lang.RuntimeException ako je parametar null ili prazan
 	 */
 	public void setPoruka(String poruka) {
-		if (this.poruka==null || this.poruka == new String("") ||
-				this.poruka.length()>140)
+		if (poruka==null || poruka.isEmpty() ||
+				poruka.length() > 140)
 			throw new RuntimeException(
 					"Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
---
+++ Twitter/test/com/twitter/poruke/TwitterPorukaTest.java
@@ -0,0 +1,85 @@
+package com.twitter.poruke;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TwitterPorukaTest {
+
+	TwitterPoruka a;
+	
+	@Before
+	public void setUp() throws Exception {
+		a = new TwitterPoruka();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		a = null;
+	}
+	/*
+	@Test
+	public void testGetKorisnik() {
+		a.setKorisnik("aleksa");
+		if(!a.getKorisnik().equals("aleksa")){
+			fail("greska!!!");
+		}
+	}*/
+
+	@Test
+	public void testSetKorisnik() {
+	a.setKorisnik("aleksa");
+	assertEquals("aleksa", a.getKorisnik());
+	}
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetKorisnikNull() {
+	a.setKorisnik(null);
+	fail("greska u set metodi, ne radi sta treba");
+	}
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetKorisnikPrazno() {
+	a.setKorisnik("");
+	fail("greska u set metodi, ne radi sta treba");
+	}
+	/*@Test
+	public void testGetPoruka() {
+		a.setPoruka("poruka");
+		if(!a.getPoruka().equals("poruka")){
+			fail("greska!!!");
+		}
+	}*/
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetPorukaNull() {
+		a.setPoruka(null);
+		fail("greska u set metodi, ne radi sta treba");
+		
+	}
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetPorukaPrazno() {
+		a.setPoruka("");
+		fail("greska u set metodi, ne radi sta treba");
+	}
+	@Test(expected = java.lang.RuntimeException.class)
+	public void testSetPorukaDuzina() {
+		a.setPoruka("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+	}
+	@Test 
+	public void testSetPoruka() {
+		a.setPoruka("nesto");
+		assertEquals("nesto", a.getPoruka());
+	}
+
+	@Test
+	public void testToString() {
+		a.setKorisnik("aleska");
+		a.setPoruka("poruka");
+		String tekst = a.toString();
+		if(!tekst.equals("KORISNIK:"+a.getKorisnik()+" PORUKA:"+a.getPoruka())){
+			fail("toString je nekako los, ubi me ako znam hahaha");
+		}
+	}
+
+}
